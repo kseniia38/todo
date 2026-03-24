39901115a4d6b04688bd2352d7d1e4cca22459fa
Ксюша Стародубцева <ksusastarodubceva@MacBook-Pro-1.IGD_Rostelecom>
2026-03-24 16:41:42 +0300

    Добавил сохранение задач в localStorage

--- js/todo.js
+++ js/todo.js
@@ -1,76 +1,118 @@
 'use strict'
 //осн константы
-const FIELD = document.querySelector('.field');
-const BUTTON = document.querySelector('.add');
-const LIST = document.querySelector('.list');
+const field = document.querySelector('.field');
+const button = document.querySelector('.add');
+const list = document.querySelector('.list');
 
 // let tasks = [];
 
 //создает задачу с чекбоксом
 
 function createTask(value) {
-    const TASK = document.createElement('div');
+    const task = document.createElement('div');
     // TASK.textContent = value;
-    TASK.classList.add('task');
+    task.classList.add('task');
 
     //
 
-    const CHECKBOX = document.createElement('input');
-    CHECKBOX.type = 'checkbox';
-    CHECKBOX.classList.add('status')
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.classList.add('status')
 
     //
 
-    const TASK_TEXT = document.createElement('div');
-    TASK_TEXT.textContent = value;
-    TASK_TEXT.classList.add('task-text');
+    const taskText = document.createElement('div');
+    taskText.textContent = value;
+    taskText.classList.add('task-text');
 
     //
 
-    const DEL = document.createElement('button')
-    DEL.textContent = 'x'
-    DEL.classList.add('del')
-    DEL.addEventListener('click', function(){
-        TASK.remove();
+    const del = document.createElement('button')
+    del.textContent = 'x'
+    del.classList.add('del')
+    del.addEventListener('click', function(){
+        task.remove();
+        saveTask();
     });
 
-    TASK.appendChild(CHECKBOX);
-    TASK.appendChild(TASK_TEXT);
-    TASK.appendChild(DEL);
+    task.appendChild(checkbox);
+    task.appendChild(taskText);
+    task.appendChild(del);
 
-    CHECKBOX.addEventListener('click', completeTask)
+    checkbox.addEventListener('click', completeTask)
     // TASK.addEventListener('click', function(){
     //     this.classList.toggle('completed')
     // })
-    return TASK;
+    return task;
 }
 
 //добавляет задачу в список на страницу
 
 function addTask() {
-    const TASK_VALUE = FIELD.value.trim();
+    const taskValue = field.value.trim();
 
-    if(TASK_VALUE) {
-        const NEW_TASK = createTask(TASK_VALUE);
-        LIST.prepend(NEW_TASK);
-        FIELD.value = '';
+    if(taskValue) {
+        const newTask = createTask(taskValue);
+        list.prepend(newTask);
+        field.value = '';
+        saveTask();
     }
 };
 
 //при нажатии на кнопку + выполняется эдтаск
 
-BUTTON.addEventListener('click', addTask)
+button.addEventListener('click', addTask)
 
 //при отмеченном чекбоксе 
 
 function completeTask(event) {
-    const CHECKBOX = event.target;
-    const TASK = CHECKBOX.parentElement;
+    const checkbox = event.target;
+    const task = checkbox.parentElement;
 
-    if(CHECKBOX.checked) {
-        TASK.classList.add('completed');
-        LIST.appendChild(TASK)
+    if(checkbox.checked) {
+        task.classList.add('completed');
+        list.appendChild(task)
     } else {
-        TASK.classList.remove('completed');
-        LIST.prepend(TASK)}
+        task.classList.remove('completed');
+        list.prepend(task)
+    }
+    saveTask()
 }
+
+//сохранение 
+
+function saveTask() {
+    const tasks = [];
+    const allTasks = document.querySelectorAll('.task');
+
+    allTasks.forEach(task => {
+        const checkbox = task.querySelector('.status')
+        const taskText = task.querySelector('.task-text');
+
+        tasks.push({
+            text: taskText.textContent,
+            completed: checkbox.checked
+        });
+    });
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+}
+
+//загрузка из localStorage
+
+function loadTask() {
+    const saved = localStorage.getItem('tasks');
+    if (saved) {
+        const tasks = JSON.parse(saved);
+        tasks.forEach(task => {
+            const newTask = createTask(task.text);
+            if(task.completed) {
+                const checkbox = newTask.querySelector('.status');
+                checkbox.checked = true;
+                newTask.classList.add('completed');
+            }
+            list.appendChild(newTask);
+        });
+    }
+}
+
+loadTask()
